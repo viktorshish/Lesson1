b8f06b2ff30a14a6cf873bd38fae71282cec4b9d
Viktor Shishkalov <viktorshish@gmail.com>
2023-02-18 00:21:05 +0300

    Tried another argument

--- price.py
+++ price.py
@@ -4,5 +4,9 @@
 def format_price(price):
     price=int(price)
     return(f'Цена: {price} руб.')
+
 fix_price = format_price(56.24)
 print(fix_price)
+
+fix_price = format_price(56.87)
+print(fix_price) # Тоже самое что и выше
