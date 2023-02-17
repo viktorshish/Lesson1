d2077a20b629280eb59479871dcafde0373492be
Viktor Shishkalov <viktorshish@gmail.com>
2023-02-18 00:20:09 +0300

    Delete brackets

--- function.py
+++ function.py
@@ -2,7 +2,7 @@
 
 # Задание №1
 def get_summ(one, two, delimiter='&'):
-    return(f'{one}{delimiter}{two}')
-    
+    return f'{one}{delimiter}{two}'
+
 vivod=get_summ('Learn','python').upper()
 print(vivod)
